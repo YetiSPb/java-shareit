893ce2d9d06d0d771e7999c7d42c86022453944a
Fedotov <fedotov-evgeny@mail.ru>
2023-08-07 16:34:37 +0300

    !
    Took 2 minutes

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import ru.practicum.shareit.item.model.Comment;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
